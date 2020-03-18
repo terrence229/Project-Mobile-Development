20a1da5f2de0be21037a72fd03ee0ebeaa95b58f
Huip van den Ende <me@huip.io>
2020-03-18 09:51:32 +0100

    finish landing page 1.0

--- client/lib/main.dart
+++ client/lib/main.dart
@@ -8,6 +8,7 @@ class MainApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      theme: ThemeData(fontFamily: 'Roboto'),
       home: LandingRoute(),
     );
   }
--- client/lib/screens/landing_route.dart
+++ client/lib/screens/landing_route.dart
@@ -3,28 +3,60 @@ import 'order_route.dart';
 
 /**
  * Splash screen
- * TODO: add login & register buttons
+ * This page only displays the title and buttons:w
+ * TODO: Add login & register buttons when work on backend startsT
  */
 class LandingRoute extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("Second Route"),
-      ),
-      body: Center(
-        child: RaisedButton(
-          onPressed: () {
-            Navigator.push(
-              context,
-              MaterialPageRoute(
-                builder: (context) {
-                  return OrderRoute();
-                },
+      backgroundColor: Color(0xFFFFE500), // FFE500 = woost color
+      // SafeArea puts widgets clear of system icons
+      body: SafeArea(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: <Widget>[
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: <Widget>[
+                Text(
+                  "To Go", // Title text
+                  style: TextStyle(
+                    fontFamily: 'Roboto',
+                    fontWeight: FontWeight.bold,
+                    fontSize: 56.0,
+                  ),
+                ),
+              ],
+            ),
+            SizedBox(height: 500) // Spacer between title & button,
+            MaterialButton(
+              minWidth: 300.0, // Hardcoded size, fix later!
+              color: Colors.black,
+              shape: RoundedRectangleBorder( // Make button rounded
+                  borderRadius: new BorderRadius.circular(50.0),
               ),
-            );
-          },
-          child: Text('Order'),
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) {
+                      return OrderRoute(); // Display new route
+                    },
+                  ),
+                );
+              },
+	      child: Text( // Button text
+                'Quick Order',
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  color: Colors.white,
+                ),
+              ),
+            ),
+          ],
         ),
       ),
     );
