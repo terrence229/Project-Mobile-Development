575bc70614716ad45f8c3124c6a1add5da87675e
Huip van den Ende <me@huip.io>
2020-03-24 19:37:24 +0100

    fix merge issues

--- client/lib/screens/landing_route.dart
+++ client/lib/screens/landing_route.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'order_route.dart';
+import 'register_route.dart';
 import 'item_menu_route.dart';
 
 /**
@@ -10,18 +12,78 @@ class LandingRoute extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("Second Route"),
-      ),
-      body: Center(
-        child: RaisedButton(
-          onPressed: () {
-            Navigator.push(
-              context,
-              MaterialPageRoute(
-                builder: (context) {
-                  return ItemMenu();
-                },
+      backgroundColor: Color(0xFFFFE500), // FFE500 = woost color
+      // SafeArea puts widgets clear of system icons
+      body: SafeArea(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: <Widget>[
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: <Widget>[
+                Text(
+                  "To Go", // Title text
+                  style: TextStyle(
+                    fontFamily: 'Roboto',
+                    fontWeight: FontWeight.bold,
+                    fontSize: 56.0,
+                  ),
+                ),
+              ],
+            ),
+            SizedBox(height: 500), // Spacer between title & button,
+            MaterialButton(
+              minWidth: 300.0, // Hardcoded size, fix later!
+              color: Colors.black,
+              shape: RoundedRectangleBorder(
+                // Make button rounded
+                borderRadius: new BorderRadius.circular(50.0),
+              ),
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) {
+                      return ItemMenu(); // Display new route
+                    },
+                  ),
+                );
+              },
+              child: Text(
+                // Button text
+                'Quick Order',
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  color: Colors.white,
+                ),
+              ),
+            ),
+            MaterialButton(
+              minWidth: 300.0, // Hardcoded size, fix later!
+              color: Colors.black,
+              shape: RoundedRectangleBorder(
+                // Make button rounded
+                borderRadius: new BorderRadius.circular(50.0),
+              ),
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) {
+                      return RegisterRoute(); // Display new route
+                    },
+                  ),
+                );
+              },
+              child: Text(
+                // Button text
+                'Register',
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  color: Colors.white,
+                ),
               ),
             ),
           ],
